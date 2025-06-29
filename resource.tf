f382317557904c2f9878970663a89045e47fe01f
unknown <yamini.pandey@ankit11mayoutlook.onmicrosoft.com>
2025-06-29 19:09:13 +0530

    finalone

---
+++ resource.tf
@@ -0,0 +1,30 @@
+
+
+
+resource "azure_resource_group" "rg" {
+  name     = "myResourceGroup"
+  location = "East US"
+}
+
+resource "azure_resource_group" "rg2" {
+  name     = "myResourceGroup2"
+  location = "East US"
+}
+
+
+
+resource "azure_storage_account" "storage" {
+  name                     = "mystorageaccount"
+  resource_group_name      = azure_resource_group.rg.name
+  location                 = azure_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azure_storage_account" "storage1" {
+  name                     = "mystorageaccount12"
+  resource_group_name      = azure_resource_group.rg.name
+  location                 = azure_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
